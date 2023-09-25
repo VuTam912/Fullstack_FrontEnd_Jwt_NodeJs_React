fc9984fbe590150f92f50778b7ef030d8ec54b16
Vu Tam R <vutam311291@gmail.com>
2023-09-25 20:51:01 +0700

    Form Login Responsive

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,32 +2,39 @@ import './Login.scss';
 
 const Login = (props) => {
 	return (
-		<div className='Login-container mt-3'>
+		<div className='Login-container '>
 			<div className='container'>
-				<div className='row'>
-					{/* md : ≥768px | */}
-					<div className='content-left col-7  '>
-						<div className=''>RYO Education</div>
+				{/* padding-x: Login */}
+				<div className='row px-3 px-sm-0'>
+					{/* sm : ≥768px | */}
+					<div className='content-left col-12 d-none col-sm-8 d-sm-block'>
+						<div className='title-brand'>RYO Education</div>
 						<div className='detail'>
-							RYO helps you connect and share with the people in your life.
+							RYO Education helps you connect and share with the people in your
+							life.
 						</div>
 					</div>
-					<div className='content-right col-5 d-flex flex-column gap-3 py-3'>
+					<div className='content-right col-sm-4 col-12 d-flex flex-column gap-3 py-3 px-3'>
+						<div className='title-brand d-sm-none'>RYO Education</div>
 						<input
 							type='text'
-							className='form-control'
+							className='form-control py-2'
 							placeholder='Email adress or phone number'
 						/>
 						<input
 							type='password'
-							className='form-control'
+							className='form-control py-2'
 							placeholder='Password'
 						/>
-						<button className='btn btn-primary fw-bold'>Login</button>
-						<span className='text-center'>Forget your password ?</span>
+						<button className='btn btn-primary fw-bold py-2 fs-5'>Login</button>
+						<span className='text-center'>
+							<a className='forget-password' href='#'>
+								Forget your password?
+							</a>
+						</span>
 						<hr />
 						<div className='text-center'>
-							<button className='btn btn-success fw-bold'>
+							<button className='btn btn-success fw-bold py-2'>
 								Create new account
 							</button>
 						</div>
